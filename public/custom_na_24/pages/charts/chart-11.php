cc2273f92ef7d5c6457cff6f4ea71584c90612ec
Fmelchor Nikken <fmelchor@nikkenlatam.com>
2024-10-09 09:32:00 -0600

    Update chart-11.php

--- public/custom_na_24/pages/charts/chart-11.php
+++ public/custom_na_24/pages/charts/chart-11.php
@@ -83,7 +83,7 @@ $datagraph2 = [];
 	<div class="row mb-3">
 		<div class="col-auto">
 			<div class="h5 fw-bold mb-1">Business Variables Report By Consultant</div>
-			<div class="h6 mb-0"><span class="fw-bold">Measurement Period:</span> January 2023 to August 2024</div>
+			<div class="h6 mb-0"><span class="fw-bold">Measurement Period:</span> January 2020 to August 2024</div>
 			<div class="h6"><span class="fw-bold">Country:</span> <?php echo $countrieUser ?></div>
 		</div>
 
